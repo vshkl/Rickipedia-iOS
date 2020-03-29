d5eb35bd84204ddf6fcb613562b2256f64007259
Pavel Vashkel <p.vashkel@datarockets.com>
2020-03-29 21:01:28 +0300

    Add EpisodeView with XIB

---
+++ Rickipedia/Custom Views/EpisodeView/EpisodeView.swift
@@ -0,0 +1,36 @@
+//
+//  EpisodeView.swift
+//  Rickipedia
+//
+//  Created by Pavel Vashkel on 3/29/20.
+//  Copyright © 2020 Pavel Vashkel. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class EpisodeView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("EpisodeView", owner: self, options: nil)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(contentView)
+        contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+
+}
