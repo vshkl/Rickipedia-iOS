fb7b15c7675342e372fb941db38df8be2ea9f492
Pavel Vashkel <p.vashkel@datarockets.com>
2020-03-29 13:45:02 +0300

    Initial Commit

---
+++ Rickipedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rickipedia
+//
+//  Created by Pavel Vashkel on 3/29/20.
+//  Copyright © 2020 Pavel Vashkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
