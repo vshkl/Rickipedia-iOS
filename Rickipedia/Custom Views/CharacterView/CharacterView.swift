e5f3ef71d10ab341ccdb1263c4bf8bf21031258a
Pavel Vashkel <p.vashkel@datarockets.com>
2020-03-30 18:57:18 +0300

    Add an ability to show characters list

---
+++ Rickipedia/Custom Views/CharacterView/CharacterView.swift
@@ -0,0 +1,56 @@
+//
+//  CharacterView.swift
+//  Rickipedia
+//
+//  Created by Pavel Vashkel on 3/30/20.
+//  Copyright © 2020 Pavel Vashkel. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+
+class CharacterView: UIView {
+    
+    var character: Character! {
+        didSet {
+            name.text = character.name
+            status.text = character.status.capitalized
+            configureImageView()
+        }
+    }
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var species: UILabel!
+    @IBOutlet weak var status: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("CharacterView", owner: self, options: nil)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(contentView)
+        contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    private func configureImageView() {
+        let url = URL(string: character.image)!
+        let placeholder = UIImage(systemName: "person.fill")!
+        
+        image.af.setImage(withURL: url, placeholderImage: placeholder)
+    }
+
+}
--- Rickipedia/Views/CharatersViewController.swift
+++ Rickipedia/Views/CharatersViewController.swift
@@ -7,11 +7,76 @@
 //
 
 import UIKit
+import Moya
 
 class CharatersViewController: UIViewController {
 
+    @IBOutlet weak var charactersTableView: UITableView!
+    
+    var characters: [Character] = []
+    var page: Int = 1
+    var pages: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "Characters"
+        
+        charactersTableView.dataSource = self
+        charactersTableView.delegate = self
+        
+        fetchCharacters()
+    }
+    
+    private func fetchCharacters(forPage page: Int = 1) {
+        let moyaProvider = MoyaProvider<APIService>()
+        
+        moyaProvider.request(.characters(page: page)) { result in
+            switch result {
+            case let .success(response):
+                do {
+                    let charactersResponse = try response.map(CharactersResponse.self)
+                    self.characters.append(contentsOf: charactersResponse.results)
+                    self.page = page
+                    self.pages = charactersResponse.info.pages
+                    self.charactersTableView.reloadData()
+                } catch let error {
+                    print(error)
+                }
+            case let .failure(error):
+                print(error)
+            }
+        }
     }
 
+}
+
+// MARK: - UITableViewDataSource
+
+extension CharatersViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath) as! CharacterCell
+        
+        cell.characterView.character = characters[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if (pages >= page && indexPath.row == characters.count - 1) {
+            fetchCharacters(forPage: page + 1)
+        }
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+
+extension CharatersViewController: UITableViewDelegate {
+    
 }
